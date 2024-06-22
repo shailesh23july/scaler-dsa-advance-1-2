abbc7de93078af6bcf9e5f01af9a81f11d28c215
Kumar <shailesh.kumar@allianz.com>
2024-06-22 09:18:52 +0530

    consecutive sum

---
+++ CombinatoricsBasics/src/scaler/dsa/maths/combinatorics/additional/problems/ConsecutiveNumbersSum.java
@@ -0,0 +1,65 @@
+package scaler.dsa.maths.combinatorics.additional.problems;
+
+public class ConsecutiveNumbersSum {
+	/*
+	Problem Description
+	Given a positive integer A.
+
+	Return the number of ways it can be written as a sum of consecutive positive integers.
+
+	Problem Constraints
+	1 <= A <= 109
+
+	Input Format
+	The first and the only argument of input contains an integer, A.
+
+	Output Format
+	Return an integer, representing the answer as described in the problem statement.
+
+	Example Input
+	Input 1:
+
+	 A = 5
+	Input 2:
+
+	 A = 15
+
+	Example Output
+	Output 1:
+	 2
+	
+	Output 2:
+	 4
+
+	Example Explanation
+	Explanation 1:
+	 The 2 ways are:
+	 => [5]
+	 => [2, 3]
+	Explanation 2:
+	 The 4 ways are:
+	 => [15]
+	 => [7, 8]
+	 => [4, 5, 6] 
+	 => [1, 2, 3, 4, 5]
+*/
+
+	public static void main(String[] args) {
+		int A = 15;
+		int solve = solve(A);
+		System.out.println(solve);
+	}
+	
+	public static int solve(int A) {
+		int N = (int) Math.pow((2*A),0.5);
+		int count = 0;
+		for (int k = 1; k <= N; k++) {
+			int T = A - (k * (k - 1))/2;
+			if (T % k == 0) {
+				count++;
+			}
+		}
+		return count;
+    }
+
+}
